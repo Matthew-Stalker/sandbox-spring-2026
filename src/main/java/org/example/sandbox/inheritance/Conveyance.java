c296dd6701789eabc89b83644b58ef2045f45259
Matthew-Stalker <MAT2291254@maricopa.edu>
2026-02-13 22:04:38 -0800

    Adding Cards

---
+++ src/main/java/org/example/sandbox/inheritance/Conveyance.java
@@ -0,0 +1,15 @@
+package org.example.sandbox.inheritance;
+
+public interface Conveyance {
+
+    void transport();
+
+    void move();
+
+    void stop();
+
+    void turnLeft();
+
+    void turnRight();
+
+}
